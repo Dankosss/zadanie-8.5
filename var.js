35f733568df4cf3dfe9413ab461108cf16656123
Michal <michalplebanski@gmail.com>
2017-09-23 20:19:51 +0200

    Commit

--- var.js
+++ var.js
@@ -1,6 +1,6 @@
 var a  = prompt('Wartosc boku "a"');
-console.log("Wartosc boku 'a' wynosi: " + a);
-var h = prompt("Wartosc wysokosci 'h'"); 
+console.log('Wartosc boku "a" wynosi: ' + a);
+var h = prompt('Wartosc wysokosci "h"'); 
 console.log ('Wartosc wysokosci "h" wynosi: ' + h);
 var triangleArea = a*h/2;
-console.log("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
